ab7daa55ca2e16dd9ef81a9d79f24e0399559287
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2018-01-17 11:15:41 -0600

    created posts model and tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'creation' do
+    before do
+      @post = Post.create(
+          date: Date.today,
+          rationale: 'This is your rationale'
+        )
+    end
+
+    it 'can be created' do
+      expect(@post).to be_valid
+    end
+
+    it 'cannot be created without date or rationale' do
+      @post.date = nil
+      @post.rationale = nil
+      expect(@post).not_to be_valid
+    end
+  end
 end
